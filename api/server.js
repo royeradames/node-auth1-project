da3e14db5ba9db4839a043b69e809199f1cd3ee1
royeradames <royeraadames@gmail.com>
2020-09-15 16:43:47 -0400

    making sure everything when up

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const helmet = require('helmet')
 const cors = require('cors')
 
 const usersRouter = require('../users/users-router.js')
-// const authRouter = require('../auth/auth-router')
 const bcryptjs = require('bcryptjs')
 const Users = require("../users/users-model.js");
 
@@ -42,7 +41,6 @@ server.use(express.json())
 server.use(cors())
 
 server.use('/api/users', authenticate, usersRouter)
-// server.use('/api/auth', authRouter)
 server.use((err, req, res, next) => {
   console.log(err.message)
   res.status(500).json({
